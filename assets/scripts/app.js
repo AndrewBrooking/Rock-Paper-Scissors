61e2279451da0b396d9e41bd8a38e2ebeb5611c8
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-27 14:23:34 -0500

    Added basic page content

---
+++ assets/scripts/app.js
@@ -0,0 +1,20 @@
+// Firebase project config
+var firebaseConfig = {
+    apiKey: "AIzaSyAD1R32X7sy5U_ICpPipU2LDiWIpcUttt0",
+    authDomain: "rock-paper-scissors-d7b85.firebaseapp.com",
+    databaseURL: "https://rock-paper-scissors-d7b85.firebaseio.com",
+    projectId: "rock-paper-scissors-d7b85",
+    messagingSenderId: "140953170879",
+    appId: "1:140953170879:web:e39d392833c3cdb7"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// Store database reference
+let database = firebase.database();
+
+// Start logic after page has loaded
+$(document).ready(function () {
+    // TODO
+});
