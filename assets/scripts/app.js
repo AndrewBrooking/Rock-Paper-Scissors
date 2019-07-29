7f3f42ab73ca770617a999bc779f3a84f70169a2
Andrew Brooking <atbrooking.dev@gmail.com>
2019-07-29 15:18:11 -0500

    Added user login and basic selection saving

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -8,25 +8,82 @@ var firebaseConfig = {
     appId: "1:140953170879:web:e39d392833c3cdb7"
 };
 
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
-
-// Store database reference
-let database = firebase.database();
-
 // Start logic after page has loaded
 $(document).ready(function () {
+
+    let database, connections;
+    let userID = "";
+    let username = "";
+    let selection = "";
+
     // Handle button click events
     $(document).on("click", "button", buttonClick);
 
     // Handle RPS selection
-    $(document).on("click", ".card", cardClick);
+    $(document).on("click", ".rps", cardClick);
 
     function buttonClick(event) {
-        // TODO
+        event.preventDefault();
+
+        // Hide the username card
+        $("#sign-in").slideUp().fadeOut();
+
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+
+        // Store database reference
+        database = firebase.database();
+
+        // Store connection reference
+        connections = database.ref("/connections");
+        connected = database.ref(".info/connected");
+
+        // Handle connection to firebase
+        connected.on("value", connect);
+
+        // Show the RPS selections
+        $("#selection").fadeIn(1500);
     }
 
     function cardClick(event) {
-        // TODO
+        // Obtain user selection
+        selection = $(this).attr("id");
+
+        console.log("Card clicked (" + selection + ")");
+
+        // Set user settings
+        let settings = {
+            username: username,
+            selection: selection
+        };
+
+        // Set user settings on Firebase
+        database.ref("/connections/" + userID).set(settings);
+
+        // TODO: Disable selecting other options
+    }
+
+    function connect(snapshot) {
+        // Check if connected
+        if (snapshot.val()) {
+
+            // Obtain username
+            username = $("#username").val().trim();
+
+            // Initalize user settings
+            let settings = {
+                username: username,
+                selection: selection
+            };
+
+            // Add user to the connections list
+            let user = connections.push(settings);
+
+            // Store userID
+            userID = user.key;
+
+            // Remove user from the connection list when they disconnect
+            user.onDisconnect().remove();
+        }
     }
 });
